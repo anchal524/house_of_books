9a1a4f6fd48122900f025163d19704b4674614ef
Anchal Bansal <anchalsingla20@gmail.com>
2022-05-04 16:24:50 -0400

    build

---
+++ client/build/precache-manifest.b3756269d0d145bc42b1cd093ec8a7b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "902f92374017ad44610c",
+    "url": "/static/css/main.90e06c9b.chunk.css"
+  },
+  {
+    "revision": "902f92374017ad44610c",
+    "url": "/static/js/main.21741b1b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5ba73d2cca42b9ad0512",
+    "url": "/static/js/2.7a21191c.chunk.js"
+  },
+  {
+    "revision": "54d2d86c8912ac7edfca761f76c2aa55",
+    "url": "/static/media/books.54d2d86c.gif"
+  },
+  {
+    "revision": "9e33baaf01015ce914874e6be42accaa",
+    "url": "/index.html"
+  }
+];
